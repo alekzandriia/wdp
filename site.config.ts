ecb20369f04b1f97b69a37622a7d9ce912fe2a6c
Alekzandriia <alekzoreillyz@me.com>
2023-04-12 23:15:42 -0400

    Update Notion Page ID
    Made sure the root Notion page is public

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'e7cbf4ff15e84b88b5fbe4284943f6db',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
